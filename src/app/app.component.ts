f3f6d8ab2fffdfe7c5f13d2dce2f1c26ddda9ef5
courtney <courtney@palski.com>
2018-12-25 23:09:23 -0500

    added push notification subscription opt in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SwPush} from '@angular/service-worker';
+import {NewsletterService} from './newsletter.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularpwa';
+  readonly VAPID_PUBLIC_KEY = 'BDB4fBZ17DSFhXmGoGu_59V3b8_6bFQObAopxjaRe5LzHAqw12G74xI4jOi8BP__9zPe0p1fBTAbAkwOj-wVYuA';
+
+  constructor(
+    private swPush: SwPush,
+    private newsletterService: NewsletterService) {
+  }
+
+  public subscribeToNotifications() {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
 }
